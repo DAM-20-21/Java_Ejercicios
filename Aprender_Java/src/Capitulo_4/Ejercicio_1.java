f8177c9750ac8c7e5c32b1a99605b3f967347be0
Jorgito <Jorgitobeta@gmail.com>
2021-01-26 15:27:11 +0100

    Subido El Ejercio 1

--- Aprender_Java/src/Capitulo_4/Ejercicio_1.java
+++ Aprender_Java/src/Capitulo_4/Ejercicio_1.java
@@ -1,9 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *Escribe un programa que pida por teclado  un dia de la semana 
+ * y que diga que asignatura toca a primera hora de ese día 
  */
 package Capitulo_4;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,4 +12,40 @@ package Capitulo_4;
  */
 public class Ejercicio_1 {
     
+    public static void main(String[] args) { 
+        String dia;
+    
+        System.out.println( "Por favor, Introduzca un dia de la semana  y le dire que asignatura le toca a primera  hora");
+    
+        /*dia = System.console().readLine();
+        dia.toLowerCase();  //Convierto a minisculas todas las letras 
+        */
+        
+        Scanner a= new Scanner(System.in); 
+        dia = a.nextLine();
+
+        
+        switch (dia){
+            case"lunes":
+                System.out.println("Matematicas");
+                break;
+            case "Martes":
+                System.out.println("Fisica");
+                break;
+            case "Miercoles":
+                System.out.println("Programacion");
+                break;
+            case "Jueves" :
+                System.out.println("Sistemas informaticos");
+                break;
+            case "Viernes" :
+                System.out.println("Base de datos");
+                break;
+            default:
+                System.out.println("No hay clase ");
+           
+        }
+        
+        
+    }
 }
