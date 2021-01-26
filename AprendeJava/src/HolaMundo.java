33436d35d45714d2a64081a2d2d29bbba00da46c
Jorgito80 <54889422+Jorgito80@users.noreply.github.com>
2021-01-26 13:40:56 +0100

    Add files via upload

---
+++ AprendeJava/src/HolaMundo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jorgito
+
+  /**
+ *
+ * @author Jorgito
+ */
+public class HolaMundo { //clase principal
+   public static void main (String[]args) {
+     System.out.println("Hola Mundo");
+
+   }
+  
+  
+}
+
+
